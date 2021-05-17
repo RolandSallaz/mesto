031bac11e67bce62282bc295f4c8be6f60db40d1
RolandSallaz <machodin@yandex.ru>
2021-05-17 19:41:33 +0300

    fix removed лайки под карточками :(

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,12 @@
-let popup = document.querySelector('.popup');
-let popupSaveButton = document.querySelector(".popup__save-button");
-let profileEditButton = document.querySelector(".profile__edit-button");
-let formCloseButton = document.querySelector(".form__close-button");
-let form = document.querySelectorAll(".form__input");
-let like = document.querySelectorAll(".element__like");
+const popup = document.querySelector('.popup');
+const popupSaveButton = document.querySelector(".popup__save-button");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const formCloseButton = document.querySelector(".form__close-button");
+const formName = document.querySelector(".form__input_name");
+const formAbout = document.querySelector(".form__input_about");
+const profileName = document.querySelector(".profile__name");
+const profileSubtitle = document.querySelector(".profile__subtitle");
 
-let profileName = document.querySelector(".profile__name");
-let profileSubtitle = document.querySelector(".profile__subtitle");
-
-
-
-profileEditButton.addEventListener("click", showEditForm);
-formCloseButton.addEventListener("click", closePopup);
-popupSaveButton.addEventListener("click", formSave);
-
-for (let index = 0; index < like.length; index++) {
-    like[index].addEventListener("click", function() {
-        like[index].classList.toggle("element__like_clicked");
-    });
-}
 
 function closePopup() {
     popup.classList.add("popup_hide");
@@ -26,12 +14,17 @@ function closePopup() {
 
 function showEditForm() {
     popup.classList.remove("popup_hide");
+    formName.value = profileName.textContent;
+    formAbout.value = profileSubtitle.textContent;
 }
 
 function formSave() {
 
     closePopup();
-    profileName.textContent = form[0].value;
-    profileSubtitle.textContent = form[1].value;
+    profileName.textContent = formName.value;
+    profileSubtitle.textContent = formAbout.value;
 
 }
+profileEditButton.addEventListener("click", showEditForm);
+formCloseButton.addEventListener("click", closePopup);
+popupSaveButton.addEventListener("click", formSave);
